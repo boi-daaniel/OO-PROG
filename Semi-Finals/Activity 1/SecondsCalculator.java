1dcce15d752daee65c05327be7ba2bbf0728dea2
boi daniel <142441884+boi-daaniel@users.noreply.github.com>
2023-10-27 13:17:54 +0800

    Update SecondsCalculator.java

--- Semi-Finals/Activity 1/SecondsCalculator.java
+++ Semi-Finals/Activity 1/SecondsCalculator.java
@@ -1,23 +1,31 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.border.Border;
 
 public class SecondsCalculator extends JFrame {
+   // Variables
    private JTextField Seconds, Years, Weeks, Hours, Minutes;
    private JButton calculateButton, exitButton;
+   private static final double SECONDS_IN_YEAR = 3.154e+7;
+   private static final double SECONDS_IN_WEEK = 604800;
+   private static final double SECONDS_IN_HOUR = 3600;
+   private static final double SECONDS_IN_MINUTE = 60;
+   private Border blackBorder = BorderFactory.createLineBorder(Color.BLACK);
+   private Color backgroundColor = new Color(0xffecd1);
 
    public SecondsCalculator() {
       // Frame Properties
-      this.setTitle("Time Calculator");
-      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      this.setSize(500, 300);
-      this.setLayout(new GridLayout(6,2));
-      this.setLocationRelativeTo(null);
-      this.getContentPane().setBackground(new Color(0x141413));
-      this.getRootPane().setBorder(BorderFactory.createLineBorder(Color.BLACK));
+      setTitle("Time Calculator");
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setSize(500, 300);
+      setLayout(new GridLayout(6,2));
+      setLocationRelativeTo(null);
+      getContentPane().setBackground(new Color(0x141413));
+      getRootPane().setBorder(blackBorder);
       Color textColor = new Color(0xfbfaf8);
       ImageIcon image = new ImageIcon("logo.png");
-      this.setIconImage(image.getImage());
+      setIconImage(image.getImage());
        
       // Seconds Panel Properties
       JLabel secondsLabel = new JLabel("Enter Seconds:");
@@ -25,65 +33,77 @@ public class SecondsCalculator extends JFrame {
       secondsLabel.setHorizontalAlignment(SwingConstants.CENTER);
       Seconds = new JTextField();
       Seconds.setHorizontalAlignment(SwingConstants.CENTER);
-      Seconds.setBackground(new Color(0xffecd1));
-      Seconds.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-
+      Seconds.setBackground(backgroundColor);
+      Seconds.setBorder(blackBorder);
       
       // Years Panel Properties
-      JLabel yearsLabel = new JLabel("Seconds to Years:");
+      JLabel yearsLabel = new JLabel("Seconds to Year:");
       yearsLabel.setForeground(textColor);
       yearsLabel.setHorizontalAlignment(SwingConstants.CENTER);
-      
       Years = new JTextField();
       Years.setHorizontalAlignment(SwingConstants.CENTER);
-      Years.setBackground(new Color(0xffecd1));
-      Years.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+      Years.setBackground(backgroundColor);
+      Years.setBorder(blackBorder);
       Years.setEditable(false);
 
       // Weeks Panel Properties
-      JLabel weeksLabel = new JLabel("Seconds to Weeks:");
+      JLabel weeksLabel = new JLabel("Seconds to Week:");
       weeksLabel.setForeground(textColor);
       weeksLabel.setHorizontalAlignment(SwingConstants.CENTER);
-      
       Weeks = new JTextField();
       Weeks.setHorizontalAlignment(SwingConstants.CENTER);
-      Weeks.setBackground(new Color(0xffecd1));
-      Weeks.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+      Weeks.setBackground(backgroundColor);
+      Weeks.setBorder(blackBorder);
       Weeks.setEditable(false);
 
       // Hours Panel Properties
-      JLabel hoursLabel = new JLabel("Seconds to Hours:");
+      JLabel hoursLabel = new JLabel("Seconds to Hour:");
       hoursLabel.setForeground(textColor);
       hoursLabel.setHorizontalAlignment(SwingConstants.CENTER);
-      
       Hours = new JTextField();
       Hours.setHorizontalAlignment(SwingConstants.CENTER);
-      Hours.setBackground(new Color(0xffecd1));
-      Hours.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+      Hours.setBackground(backgroundColor);
+      Hours.setBorder(blackBorder);
       Hours.setEditable(false);
 
       // Minutes Panel Properties
-      JLabel minutesLabel = new JLabel("Seconds to Minutes:");
+      JLabel minutesLabel = new JLabel("Seconds to Minute:");
       minutesLabel.setForeground(textColor);
       minutesLabel.setHorizontalAlignment(SwingConstants.CENTER);
       Minutes = new JTextField();
-      
       Minutes.setHorizontalAlignment(SwingConstants.CENTER);
-      Minutes.setBackground(new Color(0xffecd1));
-      Minutes.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+      Minutes.setBackground(backgroundColor);
+      Minutes.setBorder(blackBorder);
       Minutes.setEditable(false);
-        
-       
       
       // Calulate Button   
       calculateButton = new JButton("Calculate");
       calculateButton.setForeground(new Color(0x1b2021));
       calculateButton.setBackground(textColor);
+      calculateButton.setBorder(blackBorder);
       
-      calculateButton.setBorder(BorderFactory.createLineBorder(Color.BLACK));
       calculateButton.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent calculate) {
-            calculateTime();
+            try {
+            int inputSeconds = Integer.parseInt(Seconds.getText());
+
+            double years = inputSeconds / SECONDS_IN_YEAR;
+            double remainingSeconds = inputSeconds % SECONDS_IN_YEAR;
+            double weeks = remainingSeconds / SECONDS_IN_WEEK;
+            double hours = remainingSeconds / SECONDS_IN_HOUR;
+            double minutes = remainingSeconds / SECONDS_IN_MINUTE;
+
+            // Format the output of years in scientific notation
+            String formattedYears = String.format("%.4e", years);
+
+            Years.setText(formattedYears);
+            Weeks.setText(Double.toString(weeks));
+            Hours.setText(Double.toString(hours));
+            Minutes.setText(Double.toString(minutes));
+            } 
+            catch (NumberFormatException e) {
+                  JOptionPane.showMessageDialog(SecondsCalculator.this, "Invalid input. Please enter a valid number of seconds.");
+            }
          }
       });
 
@@ -91,8 +111,8 @@ public class SecondsCalculator extends JFrame {
       exitButton = new JButton("Exit");
       exitButton.setForeground(new Color(0x1b2021));
       exitButton.setBackground(textColor);
+      exitButton.setBorder(blackBorder);
       
-      exitButton.setBorder(BorderFactory.createLineBorder(Color.BLACK));
       exitButton.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent exit) {
                 System.exit(0);
@@ -117,37 +137,7 @@ public class SecondsCalculator extends JFrame {
         
       add(calculateButton);
       add(exitButton);
-      this.setVisible(true);
-   }
-
-   // Calculate Method
-   public void calculateTime() {
-      try {
-         int inputSeconds = Integer.parseInt(Seconds.getText());
-         int secondsInMinute = 60;
-         int secondsInHour = secondsInMinute * 60;
-         int secondsInDay = secondsInHour * 24;
-         int secondsInWeek = 7 * secondsInDay;
-         int secondsInYear = secondsInDay * 365;
-   
-         int years = inputSeconds / secondsInYear;
-         int remainingSeconds = inputSeconds % secondsInYear;
-         int weeks = remainingSeconds / secondsInWeek;
-         remainingSeconds %= secondsInWeek;
-         int days = remainingSeconds / secondsInDay;
-         remainingSeconds %= secondsInDay;
-         int hours = remainingSeconds / secondsInHour;
-         remainingSeconds %= secondsInHour;
-         int minutes = remainingSeconds / secondsInMinute;
-   
-         Years.setText(Integer.toString(years));
-         Weeks.setText(Integer.toString(weeks));
-         Hours.setText(Integer.toString(hours));
-         Minutes.setText(Integer.toString(minutes));
-      } 
-      catch (NumberFormatException e) {
-         JOptionPane.showMessageDialog(this, "Invalid input. Please enter a valid number of seconds.");
-      }
+      setVisible(true);
    }
 
     public static void main(String[] args) {
